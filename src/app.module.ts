6216e9f3fcb0611bfe76185db483e356eb1b724f
Mint <mintnguyen.pa@gmail.com>
2022-09-08 19:11:58 -0700

    feat: lesson type

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { LessonModule } from "./lesson/lesson.module";
+import { ApolloDriverConfig, ApolloDriver } from "@nestjs/apollo";
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true })],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			autoSchemaFile: true,
+			driver: ApolloDriver,
+		}),
+		LessonModule,
+	],
 })
 export class AppModule {}
