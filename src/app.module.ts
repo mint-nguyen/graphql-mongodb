4765d0bee19d616b25a9a618b73b09dcb685efba
Mint <mintnguyen.pa@gmail.com>
2022-09-10 14:36:28 -0700

    feat: create lesson

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
-import { Module } from "@nestjs/common";
-import { GraphQLModule } from "@nestjs/graphql";
-import { LessonModule } from "./lesson/lesson.module";
-import { ApolloDriverConfig, ApolloDriver } from "@nestjs/apollo";
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
-	imports: [
-		GraphQLModule.forRoot<ApolloDriverConfig>({
-			autoSchemaFile: true,
-			driver: ApolloDriver,
-		}),
-		LessonModule,
-	],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    LessonModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+  ],
 })
 export class AppModule {}
