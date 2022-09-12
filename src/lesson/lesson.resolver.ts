ce47755dd20705cdfaa8fdda73d5c4740cb8af0a
Mint <mintnguyen.pa@gmail.com>
2022-09-11 19:35:28 -0700

    feat: get lesson by id

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -7,13 +7,8 @@ export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'dnajdadsaf',
-      name: 'Physics',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLessonById(id);
   }
 
   @Mutation((returns) => LessonType)
